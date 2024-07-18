00d9cf8364b91ea70a9d0c33a07d7a1c89b0ccb9
sudh1994 <46997211+sudh1994@users.noreply.github.com>
2024-07-18 23:54:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ app = flask(_name_)
 @app.route('/', methods = ['get'])
 
 def home():
-  return ("Hello welcome to VCC")
+  return ("Hello welcome to VCC by G23ai2112")
 if _name_== "_main_":
  app.run(debug=true,host="0.0.0.0", port = 5000)
