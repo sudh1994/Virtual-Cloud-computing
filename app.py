f1726fe9fd7cd482b0c6a942c2b2be65f297cdd8
sudh1994 <46997211+sudh1994@users.noreply.github.com>
2024-07-19 00:01:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 #flask application
 
-from flask import flask
+from flask import Flask
 import os 
-app = flask(_name_)
+app = Flask(__name__)
 
 @app.route('/', methods = ['get'])
 
 def home():
   return ("Hello welcome to VCC by G23ai2112")
-if _name_== "_main_":
- app.run(debug=true,host="0.0.0.0", port = 5000)
+if __name__== "__main__":
+ app.run(debug=True,host="0.0.0.0", port = 5000)
